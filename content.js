4141285c29e7eec746008971b9a0e334d15505f7
RAC22 <robrac83@gmail.com>
2023-10-30 00:55:40 -0700

    prototype with update complete

--- content.js
+++ content.js
@@ -5,12 +5,20 @@ const supabaseKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtbXNhaHp0ZGVianZpZWt3c3l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0NTc5NDAsImV4cCI6MjAxNDAzMzk0MH0.EXtUNhHzwo6nxL78oLmaWS3PzV2fToEaygGZ2BMUO84";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let { data: Tanks, error } = await supabase.from("Tanks").select("*");
-console.log(error);
-console.log(Tanks);
-
-let headRow = document.getElementById("headRow");
-let body = document.getElementById("body");
+let tanks = {};
+async function getTanks() {
+	let { data: Tanks, error } = await supabase.from("Tanks").select("*");
+	tanks = Tanks;
+}
+await getTanks();
+console.log(tanks);
+tanks.sort((a, b) => {
+	return a.Al2O3 - b.Al2O3;
+});
+const modal = document.getElementById("editModal");
+const body = document.getElementById("body");
+const updateBtn = document.getElementById("update");
+const cancelBtn = document.getElementById("cancel");
 
 const order = [
 	"Product",
@@ -26,9 +34,72 @@ const order = [
 	"Updated",
 ];
 
-function buildTd(text, element) {}
-
-for (let tank of Tanks) {
+let tankMem = "";
+async function update() {
+	let forms = document.getElementsByClassName("data");
+	for (let form of forms) {
+		let prop = form.id;
+		let val = form.value;
+		tankMem[prop] = val;
+	}
+	console.log(tankMem);
+	const { data, error } = await supabase
+		.from("Tanks")
+		.update({
+			Product: tankMem.Product,
+			SG: parseFloat(tankMem.SG),
+			Al2O3: parseFloat(tankMem.Al2O3),
+			Al: parseFloat(tankMem.Al),
+			Basicity: parseFloat(tankMem.Basicity),
+			AlumPercent: parseFloat(tankMem.AlumPercent),
+			FreeAcid: parseFloat(tankMem.FreeAcid),
+			pH: parseFloat(tankMem.pH),
+			Chloride: parseFloat(tankMem.Chloride),
+			Turbidity: parseFloat(tankMem.Turbidity),
+			Updated: new Date(tankMem.Updated),
+		})
+		.eq("id", tankMem.id)
+		.select();
+	console.log(data);
+	console.log(error);
+	closeModal();
+	location.reload();
+}
+function openModal(name) {
+	let tank = tanks.filter((e) => e.Name == name)[0];
+	tankMem = tank;
+	let formEntries = document.getElementsByClassName("row");
+	for (let row of formEntries) {
+		let placeHolder = 0;
+		for (let elem of row.children) {
+			if (elem.attributes.for) {
+				let attribute = elem.attributes.for.value;
+				placeHolder = tank[attribute];
+				if (attribute == "Name") {
+					elem.innerHTML = tank[attribute].bold();
+				} else {
+					elem.innerHTML = `${attribute}`;
+				}
+			}
+			if (elem.children.length == 1) {
+				let input = elem.children[0];
+				input.value = placeHolder;
+			}
+		}
+	}
+	modal.classList.remove("hidden");
+}
+function closeModal() {
+	tankMem = "";
+	modal.classList.add("hidden");
+}
+cancelBtn.onclick = () => {
+	closeModal();
+};
+updateBtn.onclick = () => {
+	update();
+};
+for (let tank of tanks) {
 	let tr = document.createElement("tr");
 	let txt = tank.Name;
 	const name = document.createElement("th");
@@ -43,5 +114,13 @@ for (let tank of Tanks) {
 		td.appendChild(document.createTextNode(txt));
 		tr.appendChild(td);
 	}
+	let updateBtn = document.createElement("td");
+	updateBtn.setAttribute("tank", tank.Name);
+	txt = "Edit";
+	updateBtn.appendChild(document.createTextNode(txt));
+	updateBtn.onclick = () => {
+		openModal(tank.Name);
+	};
+	tr.appendChild(updateBtn);
 	body.appendChild(tr);
 }
